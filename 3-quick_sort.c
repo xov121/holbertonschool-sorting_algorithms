e795843d8c83d68177c64c2bc8ccd49346eae6d0
MMD <manuelmoralesdiaz0@gmail.com>
2024-01-16 19:08:14 -0400

    quick and selection sorts & func helpers

---
+++ 3-quick_sort.c
@@ -0,0 +1,66 @@
+#include "sort.h"
+/**
+ * quick_sort - Sorts an array of integers in ascending order using
+ * the Quick sort algorithm
+ * @array: The array to be sorted
+ * @size: The size of the array
+ */
+void quick_sort(int *array, size_t size)
+{
+    if (array == NULL || size < 2)
+        return;
+    quick_sort_helper(array, 0, size - 1, size);
+}
+/**
+ * quick_sort_helper - Sorts an array of integers in ascending order using
+ * the Quick sort algorithm
+ * @array: The array to be sorted
+ * @lo: The lowest index of the partition to sort
+ * @hi: The highest index of the partition to sort
+ * @size: The size of the array
+ */
+void quick_sort_helper(int *array, int lo, int hi, size_t size)
+{
+    int p;
+    if (lo < hi)
+    {
+        p = partition(array, lo, hi, size);
+        quick_sort_helper(array, lo, p - 1, size);
+        quick_sort_helper(array, p + 1, hi, size);
+    }
+}
+/**
+ * partition - Partitions an array of integers in ascending order using
+ * the Quick sort algorithm
+ * @array: The array to be sorted
+ * @lo: The lowest index of the partition to sort
+ * @hi: The highest index of the partition to sort
+ * @size: The size of the array
+ * Return: The index of the pivot
+ */
+int partition(int *array, int lo, int hi, size_t size)
+{
+    int pivot = array[hi], i = lo - 1, j, tmp;
+    for (j = lo; j < hi; j++)
+    {
+        if (array[j] < pivot)
+        {
+            i++;
+            if (i != j)
+            {
+                tmp = array[i];
+                array[i] = array[j];
+                array[j] = tmp;
+                print_array(array, size);
+            }
+        }
+    }
+    if (array[i + 1] > array[hi])
+    {
+        tmp = array[i + 1];
+        array[i + 1] = array[hi];
+        array[hi] = tmp;
+        print_array(array, size);
+    }
+    return (i + 1);
+}
--- sort.h
+++ sort.h
@@ -30,5 +30,7 @@ void quick_sort(int *array, size_t size); /* Task 3 */
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void swap_ints(int *a, int *b);
+void quick_sort_helper(int *array, int low, int high, size_t size);
+int partition(int *array, int low, int high, size_t size);
 
 #endif /* SORT_H */
