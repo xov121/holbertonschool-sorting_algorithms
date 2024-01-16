5177af1e226a18654315c790ba1eebff786549ec
MMD <manuelmoralesdiaz0@gmail.com>
2024-01-16 19:12:31 -0400

    fix: fixing betty style

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -7,24 +7,24 @@
  */
 void selection_sort(int *array, size_t size)
 {
-    size_t i, j, min_idx;
-    int tmp;
+	size_t i, j, min_idx;
+	int tmp;
 
-    if (array == NULL || size < 2)
-        return;
+	if (array == NULL || size < 2)
+		return;
 
-    for (i = 0; i < size - 1; i++)
-    {
-        min_idx = i;
-        for (j = i + 1; j < size; j++)
-            if (array[j] < array[min_idx])
-                min_idx = j;
-        if (min_idx != i)
-        {
-            tmp = array[i];
-            array[i] = array[min_idx];
-            array[min_idx] = tmp;
-            print_array(array, size);
-        }
-    }
+	for (i = 0; i < size - 1; i++)
+	{
+		min_idx = i;
+		for (j = i + 1; j < size; j++)
+			if (array[j] < array[min_idx])
+				min_idx = j;
+		if (min_idx != i)
+		{
+			tmp = array[i];
+			array[i] = array[min_idx];
+			array[min_idx] = tmp;
+			print_array(array, size);
+		}
+	}
 }
--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -7,9 +7,9 @@
  */
 void quick_sort(int *array, size_t size)
 {
-    if (array == NULL || size < 2)
-        return;
-    quick_sort_helper(array, 0, size - 1, size);
+	if (array == NULL || size < 2)
+		return;
+	quick_sort_helper(array, 0, size - 1, size);
 }
 /**
  * quick_sort_helper - Sorts an array of integers in ascending order using
@@ -21,13 +21,13 @@ void quick_sort(int *array, size_t size)
  */
 void quick_sort_helper(int *array, int lo, int hi, size_t size)
 {
-    int p;
-    if (lo < hi)
-    {
-        p = partition(array, lo, hi, size);
-        quick_sort_helper(array, lo, p - 1, size);
-        quick_sort_helper(array, p + 1, hi, size);
-    }
+	int p;
+	if (lo < hi)
+	{
+		p = partition(array, lo, hi, size);
+		quick_sort_helper(array, lo, p - 1, size);
+		quick_sort_helper(array, p + 1, hi, size);
+	}
 }
 /**
  * partition - Partitions an array of integers in ascending order using
@@ -40,27 +40,27 @@ void quick_sort_helper(int *array, int lo, int hi, size_t size)
  */
 int partition(int *array, int lo, int hi, size_t size)
 {
-    int pivot = array[hi], i = lo - 1, j, tmp;
-    for (j = lo; j < hi; j++)
-    {
-        if (array[j] < pivot)
-        {
-            i++;
-            if (i != j)
-            {
-                tmp = array[i];
-                array[i] = array[j];
-                array[j] = tmp;
-                print_array(array, size);
-            }
-        }
-    }
-    if (array[i + 1] > array[hi])
-    {
-        tmp = array[i + 1];
-        array[i + 1] = array[hi];
-        array[hi] = tmp;
-        print_array(array, size);
-    }
-    return (i + 1);
+	int pivot = array[hi], i = lo - 1, j, tmp;
+	for (j = lo; j < hi; j++)
+	{
+		if (array[j] < pivot)
+		{
+			i++;
+			if (i != j)
+			{
+				tmp = array[i];
+				array[i] = array[j];
+				array[j] = tmp;
+				print_array(array, size);
+			}
+		}
+	}
+	if (array[i + 1] > array[hi])
+	{
+		tmp = array[i + 1];
+		array[i + 1] = array[hi];
+		array[hi] = tmp;
+		print_array(array, size);
+	}
+	return (i + 1);
 }
